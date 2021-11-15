f5a024d5b067242c60ac0347b7410bf8ae93a914
AnnisFrancesco <84127635+AnnisFrancesco@users.noreply.github.com>
2021-11-15 10:13:29 +0100

    Add files via upload

---
+++ MovieDatabase/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+  
+    <router-view />
+  </div>
+</template>
+
+<style>
+@import "./assets/css/fontawesome.css";
+@import "./assets/css/style.css";
+@import "./assets/css/all.css";
+</style>
---
+++ MovieDatabase/src/views/AboutMovie.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <MovieDetails />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MovieDetails from "@/components/MovieDetails.vue";
+
+export default {
+  name: "Details",
+  components: {
+    MovieDetails,
+  },
+};
+</script>
