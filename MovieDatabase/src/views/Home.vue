7bc286cb58e41aae7cb4a9557ea1384d28677c34
AndreaBarducci <84127567+AndreaBarducci@users.noreply.github.com>
2021-11-15 10:43:53 +0100

    Add files via upload

---
+++ MovieDatabase/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <MovieList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MovieList from "@/components/MovieList.vue";
+
+export default {
+  name: "Home",
+  components: {
+    MovieList,
+  },
+};
+</script>
