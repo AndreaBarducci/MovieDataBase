6e8401b625850a6140aae7b4bea5a4b659f0b713
AndreaBarducci <84127567+AndreaBarducci@users.noreply.github.com>
2021-11-24 17:24:28 +0100

    Update MovieDetails.vue

--- src/components/MovieDetails.vue
+++ src/components/MovieDetails.vue
@@ -93,10 +93,11 @@
                 text-center
               "
             >
-              <img
+              <img v-if="logo.logoPath"
                 class="mx-auto object-contain w-48 h-48 rounded-xl"
                 :src="logo.bindedImage"
               />
+              <img v-else src="https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found.png" alt="poster" >
               <span class="font-semibold m-1">
                 {{ logo.name }}
               </span>
