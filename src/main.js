85fd68dda3d738a3ac9751360d60aceadbf4e843
andreabardu <barducciandrea@isisdavinci.eu>
2021-11-24 10:38:08 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ import store from "./store";
 import i18n from "./i18n";
 import "./assets/tailwind.css";
 import {createHead } from "@vueuse/head"
+import './index.css'
 createApp(App).use(store).use(router).use(i18n).use(createHead()).mount("#app");
